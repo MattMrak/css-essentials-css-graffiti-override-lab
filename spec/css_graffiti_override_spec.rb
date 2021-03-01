b807d8d2c64e833b72b305e7dc94977f629ec769
MattMrak <matthewmrakovcic@gmail.com>
2021-03-01 11:33:37 -0500

    commit tree a8899e5f9fdd7aea396a937c6b16b74c6e25b730

--- spec/css_graffiti_override_spec.rb
+++ spec/css_graffiti_override_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe 'index.html' do
   end
 end
 
-RSpec.describe 'css/cleanup.css' do
-  it 'exists' do
-    file = File.exists?("css/cleanup.css")
-    expect(file).to be_truthy, "The cleanup.css file must exist"
-  end
+# RSpec.describe 'css/cleanup.css' do
+#   it 'exists' do
+#     file = File.exists?("css/cleanup.css")
+#     expect(file).to be_truthy, "The cleanup.css file must exist"
+#   end
 
-  it 'has overrides added' do
-    minimum_bytes = 300
-    size = File.size("css/cleanup.css")
-    expect(size).to be > minimum_bytes, "You should have at least 300 bytes worth of updates to css/cleanup.css"
-  end
-end
+#   it 'has overrides added' do
+#     minimum_bytes = 300
+#     size = File.size("css/cleanup.css")
+#     expect(size).to be > minimum_bytes, "You should have at least 300 bytes worth of updates to css/cleanup.css"
+#   end
+# end
 
 RSpec.describe 'css/paint.css' do
   it 'exists' do
